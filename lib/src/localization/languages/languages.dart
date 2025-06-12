5803d575160c4849f0f91b2dc0493019ff0229f3
zbynekzundalek <zbynek.zundalek@gmail.com>
2025-06-12 18:29:52 +0900

    feat: add Japanese localization for issue messages

--- lib/src/localization/languages/languages.dart
+++ lib/src/localization/languages/languages.dart
@@ -1,2 +1,3 @@
 export 'z_issue_localization_cs.dart';
 export 'z_issue_localization_en.dart';
+export 'z_issue_localization_ja.dart';
--- test/src/localization/z_localization_context_test.dart
+++ test/src/localization/z_localization_context_test.dart
@@ -5,12 +5,16 @@ void main() {
   group('ZLocalizationContext', () {
     final csService = ZIssueLocalizationService(Language.cs);
     final enService = ZIssueLocalizationService(Language.en);
+    final jaService = ZIssueLocalizationService(Language.ja);
 
     setUp(() => ZLocalizationContext.current = ZIssueLocalizationService(defaultLanguage));
 
     test('default localization is set to ZIssueLocalizationEn', () {
       ZLocalizationContext.current = csService;
       expect(ZLocalizationContext.current, equals(csService));
+
+      ZLocalizationContext.current = jaService;
+      expect(ZLocalizationContext.current, equals(jaService));
 
       ZLocalizationContext.current = enService;
       expect(ZLocalizationContext.current, equals(enService));
