70537d9cd801f5b4d9a5611e4067c5f190fdc670
zbynekzundalek <zbynek.zundalek@gmail.com>
2025-07-03 18:24:24 +0900

    doc: add example and update README

---
+++ example/zodart_super_refine_example.dart
@@ -0,0 +1,33 @@
+// Example
+// ignore_for_file: avoid_print
+
+import 'package:zodart/zodart.dart';
+
+/// Custom superRefiner for DateTime values.
+/// Returns validation issues if the value is not in UTC or is before year 0.
+SuperRefinerErrorRes? superRefiner(DateTime val) {
+  final issues = <ZIssue>[];
+
+  if (!val.isUtc) {
+    issues.add(const ZIssueCustom(code: '10', message: 'DateTime should be in UTC'));
+  }
+
+  if (val.isBefore(DateTime(0))) {
+    issues.add(const ZIssueCustom(code: '10', message: 'DateTime should not be B.C.'));
+  }
+
+  // Converts the issues list to a SuperRefinerErrorRes if not empty; returns null otherwise.
+  // This is the expected return format for the `superRefine` method.
+  return issues.toSuperRefinerRes();
+}
+
+void main() {
+  final zDateTime = ZDateTime().superRefine(superRefiner);
+
+  final res = zDateTime.parse(DateTime(-1));
+
+  // Prints:
+  // DateTime should be in UTC
+  // DateTime should not be B.C.
+  print(res.issueSummary);
+}
