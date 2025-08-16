2e6d6f080b65804188e041f9434a3333de5510e2
zbynekzundalek <zbynek.zundalek@gmail.com>
2025-08-16 15:50:23 +0900

    test: add annotation test

---
+++ test/src/code_generation/annotation_test.dart
@@ -0,0 +1,43 @@
+import 'package:test/test.dart';
+import 'package:zodart/src/code_generation/annotations.dart';
+
+class TestOutputClass {}
+
+void main() {
+  group('ZodArt', () {
+    late ZodArt zodArt;
+    group('ZodArt.withExistingClass', () {
+      setUp(() {
+        // Using const ctor will not trigger this._(...) during the test
+        // ignore: prefer_const_constructors
+        zodArt = ZodArt.withExistingClass(outputClassType: TestOutputClass);
+      });
+
+      test('check right annotation kind', () {
+        expect(zodArt.annotationKind, equals('useExistingClass'));
+      });
+      test('check outputType', () {
+        expect(zodArt.outputType, equals(TestOutputClass));
+      });
+      test('check outputTypeStr', () {
+        expect(zodArt.outputTypeStr, isNull);
+      });
+    });
+    group('ZodArt.generateNewClass', () {
+      setUp(() {
+        // Using const ctor will not trigger this._(...) during the test
+        // ignore: prefer_const_constructors
+        zodArt = ZodArt.generateNewClass(outputClassName: 'TestOutputClass');
+      });
+      test('check right annotation kind', () {
+        expect(zodArt.annotationKind, equals('generateNewClass'));
+      });
+      test('check outputType', () {
+        expect(zodArt.outputType, isNull);
+      });
+      test('check outputTypeStr', () {
+        expect(zodArt.outputTypeStr, equals('TestOutputClass'));
+      });
+    });
+  });
+}
