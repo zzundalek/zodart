c6738da86473875fcde6bacb36ae67917cc4fcc3
zbynekzundalek <zbynek.zundalek@gmail.com>
2025-08-16 14:49:14 +0900

    feat: add spec builders

--- lib/src/code_generation/schema_parsing/schema_parsing.dart
+++ lib/src/code_generation/schema_parsing/schema_parsing.dart
@@ -1,5 +1,8 @@
 export 'refs.dart';
+export 'schema.dart';
 export 'schema_error_formatter.dart';
 export 'schema_parsing_error.dart';
+export 'spec_builder_input.dart';
+export 'spec_builder_input_visitor.dart';
 export 'z_type_converter.dart';
 export 'zodart_annotation.dart';
