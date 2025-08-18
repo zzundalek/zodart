4b20bbcbd8e080f4d337f6fda043647494d5da2e
zbynekzundalek <zbynek.zundalek@gmail.com>
2025-08-18 21:46:28 +0900

    doc: add transformation example

---
+++ example/transformations/transformations.freezed.dart
@@ -0,0 +1,145 @@
+// dart format width=80
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'transformations.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+// dart format off
+T _$identity<T>(T value) => value;
+/// @nodoc
+mixin _$Language {
+
+ String get name; String get version;
+/// Create a copy of Language
+/// with the given fields replaced by the non-null parameter values.
+@JsonKey(includeFromJson: false, includeToJson: false)
+@pragma('vm:prefer-inline')
+$LanguageCopyWith<Language> get copyWith => _$LanguageCopyWithImpl<Language>(this as Language, _$identity);
+
+
+
+@override
+bool operator ==(Object other) {
+  return identical(this, other) || (other.runtimeType == runtimeType&&other is Language&&(identical(other.name, name) || other.name == name)&&(identical(other.version, version) || other.version == version));
+}
+
+
+@override
+int get hashCode => Object.hash(runtimeType,name,version);
+
+@override
+String toString() {
+  return 'Language(name: $name, version: $version)';
+}
+
+
+}
+
+/// @nodoc
+abstract mixin class $LanguageCopyWith<$Res>  {
+  factory $LanguageCopyWith(Language value, $Res Function(Language) _then) = _$LanguageCopyWithImpl;
+@useResult
+$Res call({
+ String name, String version
+});
+
+
+
+
+}
+/// @nodoc
+class _$LanguageCopyWithImpl<$Res>
+    implements $LanguageCopyWith<$Res> {
+  _$LanguageCopyWithImpl(this._self, this._then);
+
+  final Language _self;
+  final $Res Function(Language) _then;
+
+/// Create a copy of Language
+/// with the given fields replaced by the non-null parameter values.
+@pragma('vm:prefer-inline') @override $Res call({Object? name = null,Object? version = null,}) {
+  return _then(_self.copyWith(
+name: null == name ? _self.name : name // ignore: cast_nullable_to_non_nullable
+as String,version: null == version ? _self.version : version // ignore: cast_nullable_to_non_nullable
+as String,
+  ));
+}
+
+}
+
+
+/// @nodoc
+
+
+class _Language implements Language {
+  const _Language({required this.name, required this.version});
+  
+
+@override final  String name;
+@override final  String version;
+
+/// Create a copy of Language
+/// with the given fields replaced by the non-null parameter values.
+@override @JsonKey(includeFromJson: false, includeToJson: false)
+@pragma('vm:prefer-inline')
+_$LanguageCopyWith<_Language> get copyWith => __$LanguageCopyWithImpl<_Language>(this, _$identity);
+
+
+
+@override
+bool operator ==(Object other) {
+  return identical(this, other) || (other.runtimeType == runtimeType&&other is _Language&&(identical(other.name, name) || other.name == name)&&(identical(other.version, version) || other.version == version));
+}
+
+
+@override
+int get hashCode => Object.hash(runtimeType,name,version);
+
+@override
+String toString() {
+  return 'Language(name: $name, version: $version)';
+}
+
+
+}
+
+/// @nodoc
+abstract mixin class _$LanguageCopyWith<$Res> implements $LanguageCopyWith<$Res> {
+  factory _$LanguageCopyWith(_Language value, $Res Function(_Language) _then) = __$LanguageCopyWithImpl;
+@override @useResult
+$Res call({
+ String name, String version
+});
+
+
+
+
+}
+/// @nodoc
+class __$LanguageCopyWithImpl<$Res>
+    implements _$LanguageCopyWith<$Res> {
+  __$LanguageCopyWithImpl(this._self, this._then);
+
+  final _Language _self;
+  final $Res Function(_Language) _then;
+
+/// Create a copy of Language
+/// with the given fields replaced by the non-null parameter values.
+@override @pragma('vm:prefer-inline') $Res call({Object? name = null,Object? version = null,}) {
+  return _then(_Language(
+name: null == name ? _self.name : name // ignore: cast_nullable_to_non_nullable
+as String,version: null == version ? _self.version : version // ignore: cast_nullable_to_non_nullable
+as String,
+  ));
+}
+
+
+}
+
+// dart format on
