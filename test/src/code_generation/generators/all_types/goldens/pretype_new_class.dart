fe8ab884dfef19f55c36e6b732ea0a2cfbc0a0ab
zbynekzundalek <zbynek.zundalek@gmail.com>
2025-12-22 19:00:59 +0900

    test: fix tests

--- test/src/code_generation/generators/all_types/goldens/pretype_new_class.dart
+++ test/src/code_generation/generators/all_types/goldens/pretype_new_class.dart
@@ -1,8 +1,4 @@
 part of '../pretype_src.dart';
 
-// GENERATED CODE - DO NOT MODIFY BY HAND
-// coverage:ignore-file
-// ignore_for_file: type=lint
-
 /// Class used as an output for [N.schema].
 abstract final class NewClass with _N {}
